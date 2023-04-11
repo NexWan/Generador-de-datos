1f0f3eda86c4edf94b3ad4a9de6f0793c0adf5a3
NexWan <l21051403@saltillo.tecnm.mx>
2023-04-11 03:54:44 -0600

    Normalize the text so it doesn't contain weird characters

--- src/main/java/com/nexwan/generadornombres/mainController.java
+++ src/main/java/com/nexwan/generadornombres/mainController.java
@@ -13,6 +13,7 @@ import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
 
 import java.io.*;
 import java.net.URL;
+import java.text.Normalizer;
 import java.util.*;
 import javax.swing.*;
 
@@ -190,18 +191,21 @@ public class mainController implements Initializable {
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.get(i).length; j++) {
                 if(i == list.size()-1)
-                    s[j]+=String.format("'%s');",list.get(i)[j]);
+                    s[j]+=String.format("'%s');",list.get(i)[j].toUpperCase());
                 else
-                    s[j]+=String.format("'%s',",list.get(i)[j]);
+                    s[j]+=String.format("'%s',",list.get(i)[j]).toUpperCase();
             }
         }
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < s.length; i++) {
             res.append(s[i]).append("\n");
         }
+        res = new StringBuilder(Normalizer.normalize(res, Normalizer.Form.NFD));
+        String finalS = String.valueOf(res);
+        finalS = finalS.replaceAll("[\\p{InCombiningDiacriticalMarks}]", ""); //Normalizacion del texto para remover tildes
         JScrollPane scrollPane = new JScrollPane();
         JTextArea textArea = new JTextArea();
-        textArea.append(res.toString());
+        textArea.append(finalS);
         scrollPane.add(textArea);
         JOptionPane.showMessageDialog(null,textArea,"Script resultante!",JOptionPane.INFORMATION_MESSAGE);
     }

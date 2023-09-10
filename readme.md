33639ba68368f6181b2eb672b8bd3e996e81324d
NexWan <l21051403@saltillo.tecnm.mx>
2023-09-10 00:56:47 -0600

    Cambios a la estructura del programa - Agregadas las opciones de: - - Genero - - Numero predefinido - Arreglados unos errores en el script resultante

--- readme.md
+++ readme.md
@@ -13,6 +13,8 @@ ESTE PROYECTO UTILIZA JDK 20 ASEGURATE DETENERLO DESCARGADO
 - Telefono
 - Numeros random (puede usarse como un id)
 - Correo
+- Genero
+- Numero predefinido (Si quieres que todos los registros que generes tengan algun numero como un foreign key)
 
 ## Que archivos hay?
 El archivo cuenta solamente con 2 archivos de texto, uno para los [nombres](https://github.com/NexWan/Generador-de-datos/blob/master/src/main/resources/com/nexwan/generadornombres/nombres.txt) y otro
@@ -23,18 +25,18 @@ El dato de correo es generado mediante un substring del nombre, apellido y numer
 
 ## Screenshots
 ### Interfaz Principal con sus opciones
-![Interfaz principal](https://i.imgur.com/6v9fPRj.png)
+![Interfaz principal](ImagesForGit/MainGUI.png)
 
 ### Se pregunta cuantos datos quiere generar
 ![datos](https://i.imgur.com/Dqc7qCR.png)
 
 ### Configuracion de la tabla
 Aqui en esta parte se tiene que espeficificar tomando en cuenta el nombre del atributo en tu tabla de Oracle!<br>
-![tablas](https://user-images.githubusercontent.com/89736703/230891449-5dd5de2d-e843-4dc0-834d-ff56b847b39d.png)
+![tablas](ImagesForGit/Filling.png)
 
 ### Script resultante
 Te da un script con la insercion de los datos que pediste ya formateado con su nombre de tabla y de los atributos<br>
-![image](https://user-images.githubusercontent.com/89736703/230891740-da37a940-1b95-4b9f-b6dd-61d28a976cf9.png)
+![image](ImagesForGit/Result.png)
 
 TO DO:
 - Agregar la opcion de poder agregar datos customizable.
--- src/main/java/com/nexwan/generadornombres/mainController.java
+++ src/main/java/com/nexwan/generadornombres/mainController.java
@@ -1,5 +1,6 @@
 package com.nexwan.generadornombres;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -27,8 +28,13 @@ public class mainController implements Initializable {
     public CheckBox telefono;
     public CheckBox random;
     public CheckBox correo;
+    public CheckBox numPred;
+    public CheckBox Genero;
+    public TextField numPredTF;
     @FXML
     private ImageView imageLoad;
+
+    public String[] generoArr;
 
     private boolean isVisible(){
         return hidden.isVisible();
@@ -43,96 +49,128 @@ public class mainController implements Initializable {
         imageLoad.setImage(image);
     }
 
-    public void generar() throws IOException {
-        int cantidad;
-        JTextField nombreJT = new JTextField("nombre"), apellidoPJT = new JTextField("apellidoP"), apellidoMJT = new JTextField("apellidoM"), telefonoJT = new JTextField("telefono"), correoJT = new JTextField("correo"), randomJT = new JTextField("id"), tablaJT = new JTextField("cambiame");
-        if(!(apellidoM.isSelected() || apellidoP.isSelected() || nombre.isSelected() || telefono.isSelected() || random.isSelected() || correo.isSelected())) return;
-        try{
-            cantidad = Integer.parseInt(JOptionPane.showInputDialog(null,"Cuantos datos desea generar?"));
-        }catch (Exception e){
-            JOptionPane.showMessageDialog(null,"Seleccione un numero correcto!","Error",JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-        System.out.println(apellidoP.isSelected() + "" + nombre.isSelected() + " " + apellidoM.isSelected() + "" + telefono.isSelected() + " " + random.isSelected());
-        Object[] fields= {
-                "INTRODUCE EL NOMBRE DE TUS VARIABLES EN ORACLE\n(Los campos que no seleccionaste puedes dejarlos vacios, el campo tabla NO puede estar vacio!)",
-                "nombre", nombreJT,
-                "apellido Paterno",apellidoPJT,
-                "Apellido materno",apellidoMJT,
-                "Telefono", telefonoJT,
-                "Numero random",randomJT,
-                "Correo", correoJT,
-                "Tabla", tablaJT
-        };
-        JOptionPane.showConfirmDialog(null,fields,"Selecciona",JOptionPane.OK_CANCEL_OPTION);
-        if(tablaJT.getText().equalsIgnoreCase("cambiame") || tablaJT.getText().isBlank() ){
-            JOptionPane.showMessageDialog(null,"Porfavor, dale un nombre a la tabla!!","ERROR!",JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-        System.out.println(nombreJT.getText());
-        String values = "";
-        LinkedList<String[]> toAdd = new LinkedList<>();
-        String[] names = new String[0];
+    /*Este metodo contiene toda la logica a la hora de seleccionar que datos se quierne generar*/
+    public void generar() throws IOException, RuntimeException {
+        Platform.runLater(()  ->{
+            int cantidad;
+            JTextField nombreJT = new JTextField("nombre"), apellidoPJT = new JTextField("apellidoP"), apellidoMJT = new JTextField("apellidoM"), telefonoJT = new JTextField("telefono"), correoJT = new JTextField("correo"), randomJT = new JTextField("id"), tablaJT = new JTextField("cambiame"),
+            numPredJT = new JTextField("Numero predefinido"), generoJT = new JTextField("Genero");
+            if(!(apellidoM.isSelected() || apellidoP.isSelected() || nombre.isSelected() || telefono.isSelected() || random.isSelected() || correo.isSelected())) return;
+            try{
+                cantidad = Integer.parseInt(JOptionPane.showInputDialog(null,"Cuantos datos desea generar?"));
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null,"Seleccione un numero correcto!","Error",JOptionPane.ERROR_MESSAGE);
+                return;
+            }
 
-        if (nombre.isSelected()) {
-            names = getStrings(cantidad, "nombres");
-            values += String.format("%s,", nombreJT.getText());
-            System.out.println(Arrays.toString(names));
-            toAdd.add(names);
-        }
+            Object[] fields= {
+                    "INTRODUCE EL NOMBRE DE TUS VARIABLES EN ORACLE\n(Los campos que no seleccionaste puedes dejarlos vacios, el campo tabla NO puede estar vacio!)",
+                    "nombre", nombreJT,
+                    "apellido Paterno",apellidoPJT,
+                    "Apellido materno",apellidoMJT,
+                    "Telefono", telefonoJT,
+                    "Numero random",randomJT,
+                    "Correo", correoJT,
+                    "Genero", generoJT,
+                    "Numero predefinido", numPredJT,
+                    "Tabla", tablaJT
+            };
+            JOptionPane.showConfirmDialog(null,fields,"Selecciona",JOptionPane.OK_CANCEL_OPTION);
+            if(tablaJT.getText().equalsIgnoreCase("cambiame") || tablaJT.getText().isBlank() ){
+                JOptionPane.showMessageDialog(null,"Porfavor, dale un nombre a la tabla!!","ERROR!",JOptionPane.ERROR_MESSAGE);
+                return;
+            }
 
-        if (apellidoP.isSelected()) {
-            String[] lastPName = getStrings(cantidad, "apellidos");
-            values += String.format("%s,", apellidoPJT.getText());
-            System.out.println(Arrays.toString(lastPName));
-            toAdd.add(lastPName);
-        }
+            System.out.println(nombreJT.getText());
+            String values = "";
+            LinkedList<String[]> toAdd = new LinkedList<>();
+            String[] names = new String[0];
+            try{
+                /*El codigo aqui esta un poquito curioso, simplemente verifica que atributos estan selccionados y generara esos datos
+                * PD: El atributo correo SIEMPRE tendra que ser el ultimo en ser agregado por cuestiones de diseño*/
+                if (nombre.isSelected()) {
+                    names = getStrings(cantidad, "nombres");
+                    values += String.format("%s,", nombreJT.getText());
+                    System.out.println(Arrays.toString(names));
+                    toAdd.add(names);
+                }
 
-        if (apellidoM.isSelected()) {
-            String[] lastMName = getStrings(cantidad, "apellidos");
-            values += String.format("%s,", apellidoMJT.getText());
-            System.out.println(Arrays.toString(lastMName));
-            toAdd.add(lastMName);
-        }
+                if (apellidoP.isSelected()) {
+                    String[] lastPName = getStrings(cantidad, "apellidos");
+                    values += String.format("%s,", apellidoPJT.getText());
+                    System.out.println(Arrays.toString(lastPName));
+                    toAdd.add(lastPName);
+                }
 
-        if (telefono.isSelected()) {
-            String[] nums = genTel(cantidad);
-            System.out.println(Arrays.toString(nums));
-            values += String.format("%s,", telefonoJT.getText());
-            toAdd.add(nums);
-        }
+                if (apellidoM.isSelected()) {
+                    String[] lastMName = getStrings(cantidad, "apellidos");
+                    values += String.format("%s,", apellidoMJT.getText());
+                    System.out.println(Arrays.toString(lastMName));
+                    toAdd.add(lastMName);
+                }
 
-        if (random.isSelected()) {
-            int cant = Integer.parseInt(numGen.getText());
-            String[] randoms = genRand(cantidad, cant);
-            System.out.println(Arrays.toString(randoms));
-            values += String.format("%s,", randomJT.getText());
-            toAdd.add(randoms);
-        }
+                if (telefono.isSelected()) {
+                    String[] nums = genTel(cantidad);
+                    System.out.println(Arrays.toString(nums));
+                    values += String.format("%s,", telefonoJT.getText());
+                    toAdd.add(nums);
+                }
 
-        if (correo.isSelected()) {
-            if (!nombre.isSelected())
-                names = getStrings(cantidad, "nombres");
-            String[] mails = genMail(cantidad, names, genRand(cantidad, 5), getStrings(cantidad, "apellidos"));
-            System.out.println(Arrays.toString(mails));
-            values += String.format("%s", correoJT.getText());
-            toAdd.add(mails);
-        }
+                if (random.isSelected()) {
+                    int cant = Integer.parseInt(numGen.getText());
+                    String[] randoms = genRand(cantidad, cant);
+                    System.out.println(Arrays.toString(randoms));
+                    values += String.format("%s,", randomJT.getText());
+                    toAdd.add(randoms);
+                }
 
-        generateScript(toAdd,values,tablaJT.getText(),cantidad);
+                if(numPred.isSelected()){
+                    String[] nums = new String[cantidad];
+                    values+= String.format("%s,", numPredJT.getText());
+                    Arrays.fill(nums, numPredTF.getText());
+                    toAdd.add(nums);
+                }
+
+                if(Genero.isSelected()){
+                    values+= String.format("%s,", generoJT.getText());
+                    toAdd.add(generoArr);
+                }
+
+                if (correo.isSelected()) {
+                    if (!nombre.isSelected()) names = getStrings(cantidad, "nombres");
+                    String[] mails = new String[0];
+                    mails = genMail(cantidad, names, genRand(cantidad, 5), getStrings(cantidad, "apellidos"));
+                    System.out.println(Arrays.toString(mails));
+                    values += String.format("%s", correoJT.getText());
+                    toAdd.add(mails);
+                }
+
+            }catch (IOException e){
+                JOptionPane.showConfirmDialog(null,"Ocurrio un error! intenalo denuevo");
+            }
+            generateScript(toAdd,values,tablaJT.getText(),cantidad);
+        });
     }
 
     public void showOption(){
-        hidden.setVisible(!isVisible());
+        hidden.setVisible(random.isSelected() || numPred.isSelected());
     }
 
+/*Genera los nombres llamando la funcion de createList, tambien asigna los generos de cada nombre*/
     public String[] getStrings(int cantidad, String fileName) throws IOException {
+        if(fileName.matches("nombres"))
+            generoArr = new String[cantidad];
         Random r = new Random();
         File namesFile = new File(Objects.requireNonNull(getClass().getResource(String.format("%s.txt", fileName))).getFile());
         LinkedList<String> list = createList(namesFile);
         String[] listNames = new String[cantidad];
         for (int i = 0; i < cantidad; i++) {
-            listNames[i] = list.get(r.nextInt(0, list.size() - 1));
+            int ran = r.nextInt(0, list.size() - 1);
+            if(fileName.equalsIgnoreCase("nombres")){
+                generoArr[i] = list.get(ran).substring(0,1);
+                listNames[i] = list.get(ran).substring(2);
+            }
+            else listNames[i] = list.get(ran);
         }
         return listNames;
     }
@@ -148,6 +186,7 @@ public class mainController implements Initializable {
         return list;
     }
 
+    /*Metodo para generar numeros con la lada 844*/
     public String[] genTel(int cantidad){
         Random r = new Random();
         String lada = "844";
@@ -161,6 +200,7 @@ public class mainController implements Initializable {
         return nums;
     }
 
+    /*Metodo para generar numeros aleatorios*/
     public String[] genRand(int cantidad, int max){
         String[] rand = new String[cantidad];
         Random r = new Random();
@@ -172,7 +212,8 @@ public class mainController implements Initializable {
         }
         return rand;
     }
-    
+
+    /*Metodo para generar correos electronicos basados en el nombre y apellido*/
     public String[] genMail(int cantidad, String[] nombre, String[] rand, String[] apellido){
         String[] mails = new String[cantidad];
         Random r = new Random();
@@ -182,24 +223,24 @@ public class mainController implements Initializable {
         return mails;
     }
 
+    /**/
     public void generateScript(LinkedList<String[]> list, String values, String table, int cantidad){
         String[] s = new String[cantidad];
         if(!correo.isSelected())
             values = values.substring(0,values.length()-1);
-        for (int i = 0; i < cantidad; i++) {
-            s[i]=String.format("INSERT INTO %s (%s) VALUES(",table,values);
-        }
+        Arrays.fill(s, String.format("INSERT INTO %s (%s) VALUES(", table, values));
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.get(i).length; j++) {
-                if(i == list.size()-1)
-                    s[j]+=String.format("'%s');",list.get(i)[j].toUpperCase());
-                else
-                    s[j]+=String.format("'%s',",list.get(i)[j]).toUpperCase();
+                String value = list.get(i)[j];
+                System.out.println(value);
+                boolean isNumeric = value.matches("\\d+");
+
+                s[j] += (isNumeric ? "" : "'") + value.toUpperCase() + (i == list.size() - 1 ? (isNumeric ? ") " : "')") : (isNumeric ? ", " : "',"));
             }
         }
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < s.length; i++) {
-            res.append(s[i]).append("\n");
+        for (String string : s) {
+            res.append(string).append("\n");
         }
         res = new StringBuilder(Normalizer.normalize(res, Normalizer.Form.NFD));
         String finalS = String.valueOf(res);
